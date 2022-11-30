7bdf61f445a745322bebe8139eb8638e627ea2a4
PLESSI Nicolo <nicolo.plessi@soprasteria.com>
2022-11-30 12:48:55 +0100

    primo commit

--- src/main/java/com/soprasteria/terna/mymicroservice/utenti/service/UtentiService.java
+++
@@ -1,8 +0,0 @@
-package com.soprasteria.terna.mymicroservice.utenti.service;
-
-import com.soprasteria.terna.mymicroservice.utenti.api.model.UtenteDto;
-
-public interface UtentiService {
-    
-    public UtenteDto getUtentiByUsername(String username);
-}
